4386548510d512fd535b591d09010a3c968c9b0d
Sebe2k04 <sebe2k04@gmail.com>
2025-11-05 23:20:01 +0530

    feat:update

--- apps/backend/src/user/user.module.ts
+++
@@ -1,6 +0,0 @@
-import type { FastifyInstance } from "fastify"
-import { userController } from "./user.controller"
-
-export const userModule = async (app: FastifyInstance) => {
-  await userController(app)
-}
